9b2427d9f1d08f20bc1b1771fcb6063d51b64581
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-02 23:38:16 +0600

    post method added

--- src/components/AddUsers.jsx
+++ src/components/AddUsers.jsx
@@ -7,11 +7,24 @@ const AddUsers = () => {
 
     const handleUser = e => {
         e.preventDefault();
+
+        fetch('http://localhost:5000/user',{
+            method: 'PUT',
+            headers: {
+                'content-type':'application/json'
+            },
+            body : JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
     }
 
     const handleInputBlur = e =>{
         const value = e.target.value;
         const field = e.target.name;
+        const newUser = {...user}
+        newUser[field] = value;
+        setUser(newUser);
         
     }
 
@@ -19,9 +32,9 @@ const AddUsers = () => {
         <div>
             <h2>Please Add A New User</h2>
             <form onSubmit={handleUser}>
-                <input onBlur={handleInputBlur} type="text" name="name" placeholder="Name" />
+                <input onBlur={handleInputBlur} type="text" name="name" placeholder="Name" required />
                 <br />
-                <input onBlur={handleInputBlur} type="email" name="email" placeholder="Email" />
+                <input onBlur={handleInputBlur} type="email" name="email" placeholder="Email" required />
                 <br />
                 <button type='submit'>Add User</button>
 
