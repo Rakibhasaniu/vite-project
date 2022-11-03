a9795974d486e71fabb932cfddde4b5ea6deaa6e
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-03 15:50:24 +0600

    delete option added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const user = useLoaderData();
+    const handleDelete = _id => {
+        const agree = window.confirm('Are You Sure');
+        console.log(agree)
+        if(agree){
+
+            console.log("deleting user with id", _id)
+        }
+    }
     return (
         <div>
-            <h2>Users:</h2>
+            <h2>Users:{user.length}</h2>
+            <div>
+                {
+                    user.map(user => <p key={user._id}>
+                        {user.name} {user.email} <button onClick={() => handleDelete(user._id)}>X</button>
+                    
+                    </p>)
+                }
+            </div>
             
         </div>
     );
