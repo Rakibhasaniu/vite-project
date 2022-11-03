2367e1afc55b82869c9b54701020e166c3839518
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-03 15:10:46 +0600

    crud = create done

--- src/components/AddUsers.jsx
+++ src/components/AddUsers.jsx
@@ -8,15 +8,20 @@ const AddUsers = () => {
     const handleUser = e => {
         e.preventDefault();
 
-        fetch('http://localhost:5000/user',{
-            method: 'PUT',
+        fetch('http://localhost:5000/users',{
+            method: 'POST',
             headers: {
                 'content-type':'application/json'
             },
             body : JSON.stringify(user)
         })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            if(data.acknowledged){
+                alert('User Added Successfully')
+                e.target.reset();
+            }
+        })
     }
 
     const handleInputBlur = e =>{
